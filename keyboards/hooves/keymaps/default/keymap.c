f86f148f8acc83e25f65b8f079192bce5b9e12b8
fukayatsu <fukayatsu@gmail.com>
2019-11-22 17:39:29 +0900

    Tweak keymap

--- keyboards/hooves/keymaps/default/keymap.c
+++ keyboards/hooves/keymaps/default/keymap.c
@@ -24,30 +24,31 @@ enum layer_names {
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
+#define T__ KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT( \
-/**/  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
-/**/  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-/**/  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-/**/                    KC_GRV,  KC_BSLS,                                         KC_LBRC, KC_RBRC,                   \
-/**/                             KC_SPC,  KC_LCTL, KC_LALT,     KC_RALT, KC_BSPC, KC_ENT,                             \
-/**/                             RAISE,   KC_LGUI, KC_LSFT,     KC_RSFT, KC_RGUI, LOWER                            \
+/**/  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
+/**/  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+/**/  KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, \
+/**/                    KC_PLUS, KC_PEQL,                                         KC_LBRC, KC_RBRC,                   \
+/**/                             KC_LCTL, KC_SPC, KC_LALT,      KC_RALT, KC_ENT, KC_BSPC,                             \
+/**/                             RAISE,   KC_LGUI, KC_LSFT,     KC_RSFT, KC_RGUI, LOWER                               \
 ),
 [_RAISE] = LAYOUT( \
-/**/  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, \
-/**/  KC_LCTL, ___,     ___,     ___,     ___,     ___,         ___,     ___,     KC_LCBR, KC_RCBR, KC_COLN, KC_DQUO, \
-/**/  KC_LSFT, ___,     ___,     ___,     ___,     RESET,       ___,     ___,     KC_LT,   KC_GT,   KC_QUES, KC_BSLS, \
-/**/                    KC_GRV,  KC_BSLS,                                         KC_LBRC, KC_RBRC,                   \
-/**/                             KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,                            \
-/**/                             KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS                             \
+/**/  T__,     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, \
+/**/  T__,     ___,     ___,     ___,     ___,     ___,         KC_PLUS, KC_PEQL, KC_LCBR, KC_RCBR, KC_COLN, KC_DQUO, \
+/**/  T__,     ___,     ___,     ___,     ___,     RESET,       KC_PIPE, KC_GRV,  KC_LT,   KC_GT,   KC_QUES, T__,     \
+/**/                    T__,     T__,                                             KC_LBRC, KC_RBRC,                   \
+/**/                             T__,     T__,     T__,         T__,     T__,     T__,                                \
+/**/                             T__,     T__,     T__,         T__,     T__,     T__                                 \
 ),
 [_LOWER] = LAYOUT( \
-/**/  KC_ESC,  ___,     ___,     ___,     ___,     ___,         ___,     ___,     ___,     ___,     ___,     KC_BSPC, \
-/**/  KC_LCTL, KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,       KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,   ___,     \
-/**/  KC_LSFT, ___,     ___,     ___,     ___,     ___,         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, ___,     KC_RSFT,  \
+/**/  T__,     ___,     ___,     ___,     ___,     ___,         ___,     ___,     ___,     ___,     ___,     ___,     \
+/**/  T__,     KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,       KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,   ___,     \
+/**/  T__,     ___,     ___,     ___,     ___,     ___,         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, ___,     ___,     \
 /**/                    ___,     ___,                                             ___,     ___,                       \
-/**/                             KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,                            \
-/**/                             KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS                             \
+/**/                             T__,     T__,     T__,         T__,     T__,     T__,                                \
+/**/                             T__,     T__,     T__,         T__,     T__,     T__                                 \
 )
 };
