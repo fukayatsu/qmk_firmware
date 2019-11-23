aebf6b4dbfe0c0fcea5bb14810ab89095ae7e65e
fukayatsu <fukayatsu@gmail.com>
2019-11-23 15:12:16 +0900

    Tweak keymap

--- keyboards/hooves/keymaps/default/keymap.c
+++ keyboards/hooves/keymaps/default/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /**/  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
 /**/  KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, \
 /**/                    KC_PLUS, KC_PEQL,                                         KC_LBRC, KC_RBRC,                   \
-/**/                             KC_LCTL, KC_SPC, KC_LALT,      KC_RALT, KC_ENT, KC_BSPC,                             \
+/**/                             KC_BSPC, KC_LCTL, KC_LALT,     KC_RALT, KC_ENT,  KC_SPC,                             \
 /**/                             RAISE,   KC_LGUI, KC_LSFT,     KC_RSFT, KC_RGUI, LOWER                               \
 ),
 [_RAISE] = LAYOUT( \
